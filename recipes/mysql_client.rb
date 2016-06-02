90fd08135f35ef68ac247c0573e4d33a545be0dd
Puja Salampuria <pujasalampuria@gmail.com>
2016-06-02 19:28:10 +0530

    added mysql_client recipe

--- recipes/mysql_client.rb
+++ recipes/mysql_client.rb
@@ -1,2 +1,12 @@
 # install mysql-client
+
+directory '/var/lib/jenkins/files' do
+  action :create
+end
+
+remote_file "/var/lib/jenkins/files/backup-magento-testnow.sql" do
+  source "https://s3.amazonaws.com/spirentmysql/backup-magento-testnow.sql"
+  action :create
+end
+
 package 'mysql-client-5.5'
